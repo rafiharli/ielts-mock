986d622309f926680fc42b6fd7fff6833e172ee2
rafiharli <135038628+rafiharli@users.noreply.github.com>
2023-12-20 08:00:31 +0700

    Create db_ielts.sql

---
+++ db/db_ielts.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2023 at 01:55 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_ielts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listening`
+--
+-- Error reading structure for table db_ielts.listening: #1932 - Table &#039;db_ielts.listening&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_ielts.listening: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ielts`.`listening`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reading`
+--
+-- Error reading structure for table db_ielts.reading: #1932 - Table &#039;db_ielts.reading&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_ielts.reading: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ielts`.`reading`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writing`
+--
+-- Error reading structure for table db_ielts.writing: #1932 - Table &#039;db_ielts.writing&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_ielts.writing: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ielts`.`writing`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
